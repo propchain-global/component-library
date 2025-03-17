e813d9993cd487fc68167d4697b828ad34f37946
victorprop <v.adelowo@propchain.com>
2025-03-17 11:11:15 +0100

    minor changes

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -12,7 +12,25 @@ const kodeMono = Kode_Mono({
   subsets: ["latin"],
 });
 
+
+
 const preview: Preview = {
+  initialGlobals: {
+    dataThemes: {
+      list: [
+        { name: "Welf", dataTheme: "welf", color: "#54b342" },
+        { name: "PropChain", dataTheme: "propchain", color: "#FFBE06" },
+      ],
+      dataAttribute: "data-theme",            // optional (default: "data-theme")
+      clearable: true,                        // optional (default: true)
+      toolbar: {
+        title: "Change data-theme attribute", // optional
+        icon: "PaintBrushIcon",               // optional
+      },
+    },
+    dataTheme: "welf",
+
+  },
   parameters: {
     controls: {
       matchers: {
